087c029816a3e1bc59aa1c48052ec582b53f9073
Gogetanks <88531280+Gogetanks@users.noreply.github.com>
2023-03-13 16:44:26 +0100

    Add files via upload

---
+++ UnitTestProject1/StringCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class StringCalculator
+    {
+        public static int Calculate(string arg = null)
+        {
+            if (arg == null)
+                return 0;
+            
+            if (arg.Contains(","))
+            {
+                string[] numbers = arg.Split(',');
+
+                return int.Parse(numbers[0]) + int.Parse(numbers[1]);
+            }
+
+            if (arg.Contains("\n"))
+            {
+                string[] numbers = arg.Split('\n');
+
+                return int.Parse(numbers[0]) + int.Parse(numbers[1]);
+            }
+
+            if (arg.Contains("\n") || arg.Contains(','))
+            {
+                int sum = 0;
+               
+                Char[] delimiters = { ',', '\n' };
+                string[] numbers = arg.Split(delimiters);
+
+                for (int i = 0;  i < numbers.Length; i++)
+                 sum += int.Parse(numbers[i]);
+
+                return sum;
+            }
+
+            if (arg.Contains("-"))
+                throw new IndexOutOfRangeException("Negative number");
+
+            return int.Parse(arg);
+
+        }
+    }
+}
